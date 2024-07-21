edc9c410698f554445fd5ccd913df0db5dc7438f
Daniel Moreira Perera <117321635+DaniMore96@users.noreply.github.com>
2024-07-21 16:56:34 -0300

    Condicionales.sql

--- Condicionales.sql
+++ Condicionales.sql
@@ -3,8 +3,8 @@ SELECT
 	titulo,
 	año_lanzamiento,
     CASE
-		WHEN año_lanzamiento >= 2020 THEN 'NUEVA'
-		WHEN año_lanzamiento BETWEEN 2010 AND 2019 THEN 'CLASICA'
+	WHEN año_lanzamiento >= 2020 THEN 'NUEVA'
+	WHEN año_lanzamiento BETWEEN 2010 AND 2019 THEN 'CLASICA'
         ELSE 'ANTIGUA'
         END AS 'CATEGORIA'
 	FROM Series
@@ -12,8 +12,8 @@ SELECT
 -- Condicionales IF
 SELECT titulo, año_lanzamiento,
 	IF(año_lanzamiento >= 2014, 'Plazo', 'Fuera') AS 'Categoria'
-    FROM Series
-    ORDER BY año_lanzamiento DESC;
+    	FROM Series
+    	ORDER BY año_lanzamiento DESC;
 
 -- EJERCICIO
 -- Escribe una consulta SQL que seleccione el titulo de todas las series de la tabla Series junto con una nueva columna denominada 'Antigüedad'.
@@ -23,9 +23,9 @@ SELECT titulo, año_lanzamiento,
 SELECT titulo,
 	CASE
 		WHEN año_lanzamiento < 2010 THEN 'Antigua'
-        WHEN año_lanzamiento >= 2010 THEN 'Reciente'
-        ELSE 'NO TIENE AÑO DE LANZAMIENTO'
-        END AS 'Antigüedad'
-	FROM Series
-    ORDER BY año_lanzamiento;
+        	WHEN año_lanzamiento >= 2010 THEN 'Reciente'
+        	ELSE 'NO TIENE AÑO DE LANZAMIENTO'
+        	END AS 'Antigüedad'
+		FROM Series
+    		ORDER BY año_lanzamiento;
 
